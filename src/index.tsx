3d6eec557737fc2028a3352989869d6cfdc47951
SaharovNA <166331345+SaharovNA@users.noreply.github.com>
2024-10-21 19:11:46 +0300

    Revert "Homework5 (#2)"
    This reverts commit cd42a8a824406943b5cda768c464d20b6c106aae.

--- src/index.tsx
+++ src/index.tsx
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './app/index.css';
 import App from './app/App';
 
-import './components/providers/LangProvider/i18n';
-
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
   <React.StrictMode>
