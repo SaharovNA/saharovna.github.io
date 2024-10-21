7e2df04609d041083444cfb9e544567a1330f11c
SaharovNA <166331345+SaharovNA@users.noreply.github.com>
2024-10-21 19:12:15 +0300

    Revert "Homework5 (#2)" (#3)
    This reverts commit cd42a8a824406943b5cda768c464d20b6c106aae.

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -3,15 +3,11 @@ import React, { FC } from 'react';
 //import { Logo } from 'src/components/Logo/Logo';
 import { Logo } from '../Logo/Logo';
 import styles from './Header.module.css';
-import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher';
-import { LangSwitcher } from '../LangSwitcher/LangSwitcher';
 
 export const Header: FC = () => {
   return (
     <header className={styles.header}>
       <Logo />
-      <ThemeSwitcher />
-      <LangSwitcher />
     </header>
   );
 }
