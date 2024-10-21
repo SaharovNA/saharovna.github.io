1d010a39da65b7b664e251be422758ea7883f7a3
n.saharov <n.saharov@dt1520.ru>
2024-10-21 18:19:17 +0300

    feat: Компоненты провайдера переключения языков
    Провайдер и компонент переключения языков.
    Провайдер подключен к приложению в компонент заголовка.
    Подключение переключателя языков в сторибук.
    Подключение библиотеки i18next в проект.

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -1,7 +1,10 @@
 import React from 'react';
+//import { I18nextProvider } from 'react-i18next';
+import { useTranslation, Trans } from 'react-i18next';
 import logo from './logo.svg';
 import './App.css';
 import { ThemeProvider } from '../components/providers/ThemeProvider/ThemeProvider';
+import { LangProvider } from '../components/providers/LangProvider/LangProvider';
 import '../components/providers/ThemeProvider/themes.css';
 //import { Logo } from 'src/components/Logo/Logo';
 import { Header } from '../components/Header/Header';
@@ -13,9 +16,16 @@ import { Header } from '../components/Header/Header';
 //import { ProductDescFull } from 'src/components/ProductDescFull/ProductDescFull';
 
 function App() {
+  const { t } = useTranslation();
+
   return (
     <ThemeProvider>
       {/* <ThemeProvider> */}
+        <LangProvider>
+          {/* <LangProvider> */}
+        {/* <I18nextProvider i18n={i18n}>
+          <LangProvider> */}
+
           <div className="App">
             <header className="App-header">
             <Header />
@@ -29,20 +39,20 @@ function App() {
 
               <img src={logo} className="App-logo" alt="logo" />
               <p>
-                Меня зовут Николай, я ведущий инженер-программист в компании ООО &quot;Цифровые технологии 1520&quot; (<a>https://dt1520.ru</a>).
-                <br/>
-                Я более 20 лет занимаюсь разработкой автоматизированных систем для ОАО &quot;РЖД&quot;, АО &quot;Росжелдорпроект&quot; и других проектных институтов на ж.д. транспорте (РЖД, метро и пр.).
-                <br/>
-                Основные мои направления: АС ФС (система формирования спецификаций оборудования и материалов), АСУ-Ш-2 (автоматизированная система управления хозяйством СЦБ на ж.д. транспорте).
-                <br/>
-                Применяемые в разработке языки и технологии: C#, VB.NET, Delphi, .NET Framework, .NET Core, JavaScript, ExtJS Framework, MSSQL, PostgreSQL, SVN.
-                <br/>
-                Мне хотелось бы познакомиться с современными направлениями и технологиями разработки, лучше узнать использование git, unit-тестирование и т.д.
-                <br/>
-                Мой телеграм: @sakharov_n_a
+                <Trans i18nKey="description.part1">
+                  Edit <code>src/App.js</code> and save to reload.
+                </Trans>
+              </p>
+              <p>
+                {t('description.part2')}
               </p>
             </header>
           </div>
+
+          {/* </LangProvider>
+          </I18nextProvider> */}
+          {/* </LangProvider> */}
+        </LangProvider>
       {/* </ThemeProvider> */}
     </ThemeProvider>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -4,12 +4,14 @@ import React, { FC } from 'react';
 import { Logo } from '../Logo/Logo';
 import styles from './Header.module.css';
 import { ThemeSwitcher } from '../ThemeSwitcher/ThemeSwitcher';
+import { LangSwitcher } from '../LangSwitcher/LangSwitcher';
 
 export const Header: FC = () => {
   return (
     <header className={styles.header}>
       <Logo />
       <ThemeSwitcher />
+      <LangSwitcher />
     </header>
   );
 }
